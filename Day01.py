aa96cb5d6b7058590a2d55987d58b374764d2fcd
lalithalalithat14@gmail.com <youremail@example.com>
2025-12-11 22:42:35 +0530

    Initial commit

---
+++ Day01.py
@@ -0,0 +1,22 @@
+#Array example
+arr = [10, 20, 30, 40, 50]
+print("Array:", arr)
+
+#array Access element
+print("First element:", arr[0])
+print("Last element:", arr[-1])
+
+#update a value
+arr[2] = 99
+print("After update:", arr)
+
+#Add an element
+arr.append(60)
+print("After adding:", arr)
+
+#Remove an element
+arr.remove(20)
+print("After removing 20:", arr)
+
+#length of an array
+print("Length:", len(arr))
